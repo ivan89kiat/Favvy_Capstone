9512effa03769bf86a7003d7484033bc4e901ac5
ivan89kiat <ivan89kiat@gmail.com>
2023-05-08 12:04:59 +0800

    added api to call bank transaction history of user

--- src/Components/History.js
+++ src/Components/History.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import BankAuth from "./BankAuth";
 
 export default function History() {
-  return <div>This is history</div>;
+  const [userAccessToken, setUserAccessToken] = useState();
+  const clientId = "6l55vZudIzUORCzfe1Q4AVNrumMa";
+  const callBackUrl = "http://localhost:3000";
+  const accessToken = "bdb1058a-c248-37eb-a571-bae442a48051";
+
+  useEffect(() => {}, []);
+
+  const getTransaction = async () => {
+    console.log("this is running");
+    await axios.post(
+      `https://api.ocbc.com:8243/transactional/corp/account/1.0/history`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+  };
+
+  return (
+    <div>
+      This is history
+      <BankAuth />
+    </div>
+  );
 }
