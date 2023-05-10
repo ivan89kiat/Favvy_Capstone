c5404399df50215bbbff17ab42e3b560c28da305
ivan89kiat <ivan89kiat@gmail.com>
2023-05-10 21:57:39 +0800

    link to get authorization from bank

--- src/Components/BankAuth.js
+++ src/Components/BankAuth.js
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 export default function BankAuth() {
-  const handleAuthClick = () => {
-    axios
-      .get(
-        "https://api.ocbc.com/ocbcauthentication/api/oauth2/authorize?client_id=6l55vZudIzUORCzfe1Q4AVNrumMa&redirect_uri=http://localhost:8080&scope=transactional"
-      )
-      .then((res) => {
-        console.log(res.data);
-        let popupWindow = window.open(
-          "",
-          "popupWindow",
-          "width=600,height=600,scrollbars=yes"
-        );
-
-        popupWindow.document.write(res.data);
-      });
-  };
-
   return (
     <div>
       {/* <button onClick={handleAuthClick}>Authorize with Bank</button> */}
       <div>
-        <a href="https://api.ocbc.com/ocbcauthentication/api/oauth2/authorize?client_id=6l55vZudIzUORCzfe1Q4AVNrumMa&redirect_uri=http://localhost:8080&scope=transactional">
+        <a href="https://api.ocbc.com/ocbcauthentication/api/oauth2/authorize?client_id=6l55vZudIzUORCzfe1Q4AVNrumMa&redirect_uri=http://localhost:3000&scope=transactional">
           Authorize with Bank
         </a>
       </div>
